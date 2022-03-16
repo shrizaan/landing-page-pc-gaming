4545f407d43842a8dbabf217fc1b992f81a720b7
shrizaan <85440492+shrizaan@users.noreply.github.com>
2022-03-16 09:51:31 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.intro-image-container', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
+
+
+
+
